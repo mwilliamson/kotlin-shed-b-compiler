46f1b72fcb0473a267a249d2be18c58dc2e98d4e
Michael Williamson <mike@zwobble.org>
2017-04-14 18:06:56 +0100

    Extract compileModule in Python backend

--- backend-python/src/test/kotlin/org/shedlang/compiler/backends/python/tests/ExecutionTests.kt
+++ backend-python/src/test/kotlin/org/shedlang/compiler/backends/python/tests/ExecutionTests.kt
@@ -4,11 +4,13 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
+import org.shedlang.compiler.ast.ModuleNode
 import org.shedlang.compiler.backends.python.generateCode
 import org.shedlang.compiler.backends.python.serialise
 import org.shedlang.compiler.backends.tests.run
 import org.shedlang.compiler.backends.tests.testPrograms
 import org.shedlang.compiler.read
+import org.shedlang.compiler.typechecker.ResolvedReferences
 import org.shedlang.compiler.typechecker.TypeCheckError
 
 class ExecutionTests {
@@ -17,12 +19,7 @@ class ExecutionTests {
         return testPrograms().map({ testProgram -> DynamicTest.dynamicTest(testProgram.name, {
             try {
                 val frontendResult = read(filename = "<string>", input = testProgram.source)
-
-                val generateCode = generateCode(frontendResult.module, frontendResult.references)
-                val stdlib = """\
-                    int_to_string = str
-                """.trimMargin()
-                val contents = stdlib + "\n" + serialise(generateCode) + "\nmain()\n"
+                val contents = compileModule(frontendResult.module, frontendResult.references)
                 val result = run(listOf("python", "-c", contents))
                 assertThat(result, equalTo(testProgram.expectedResult))
             } catch (error: TypeCheckError) {
@@ -30,5 +27,14 @@ class ExecutionTests {
                 throw error
             }
         }) })
+    }
+
+    private fun compileModule(module: ModuleNode, references: ResolvedReferences): String {
+        val generateCode = generateCode(module, references)
+        val stdlib = """\
+            int_to_string = str
+        """.trimMargin()
+        val contents = stdlib + "\n" + serialise(generateCode) + "\nmain()\n"
+        return contents
     }
 }
