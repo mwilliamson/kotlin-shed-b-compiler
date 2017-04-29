743e9614f790c23154405960da7b95e63f62277a
Michael Williamson <mike@zwobble.org>
2017-04-29 17:33:22 +0100

    Support multiple-module test programs in Python backend

--- backend-python/src/test/kotlin/org/shedlang/compiler/backends/python/tests/ExecutionTests.kt
+++ backend-python/src/test/kotlin/org/shedlang/compiler/backends/python/tests/ExecutionTests.kt
@@ -4,18 +4,19 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
-import org.shedlang.compiler.ast.ModuleNode
+import org.shedlang.compiler.Module
 import org.shedlang.compiler.backends.python.generateCode
 import org.shedlang.compiler.backends.python.serialise
 import org.shedlang.compiler.backends.tests.run
 import org.shedlang.compiler.backends.tests.temporaryDirectory
 import org.shedlang.compiler.backends.tests.testPrograms
+import org.shedlang.compiler.identifyModule
 import org.shedlang.compiler.read
-import org.shedlang.compiler.typechecker.ResolvedReferences
 import org.shedlang.compiler.typechecker.TypeCheckError
 import java.io.File
 import java.io.Writer
 import java.nio.charset.StandardCharsets
+import java.nio.file.Path
 
 class ExecutionTests {
     @TestFactory
@@ -27,19 +28,24 @@ class ExecutionTests {
                         base = testProgram.base,
                         path = testProgram.main
                     )
-                    val module = frontendResult.modules.single()
-                    val moduleName = module.path
-                    val modulePath = moduleName.joinToString(File.separator) + ".py"
-                    val destination = temporaryDirectory.file.resolve(modulePath)
-                    destination.writer(StandardCharsets.UTF_8).use { writer ->
-                        compileModule(
-                            module = module.node,
-                            references = module.references,
-                            writer = writer
-                        )
-                    }
+                    frontendResult.modules.forEach({ module ->
+                        val moduleName = module.path
+                        val modulePath = moduleName.joinToString(File.separator) + ".py"
+                        val destination = temporaryDirectory.file.resolve(modulePath)
+                        val pythonPackage = destination.toPath().parent
+                        pythonPackage.toFile().mkdirs()
+                        addInitFiles(temporaryDirectory.file.toPath(), pythonPackage)
+
+                        destination.writer(StandardCharsets.UTF_8).use { writer ->
+                            compileModule(
+                                module = module,
+                                writer = writer
+                            )
+                        }
+                    })
+                    val mainModule = identifyModule(testProgram.main).joinToString(".")
                     val result = run(
-                        listOf("python", "-m", moduleName.joinToString(".")),
+                        listOf("python", "-c", "from ${mainModule} import main; main()"),
                         workingDirectory = temporaryDirectory.file
                     )
                     assertThat(result, equalTo(testProgram.expectedResult))
@@ -51,16 +57,20 @@ class ExecutionTests {
         }) })
     }
 
-    private fun compileModule(
-        module: ModuleNode,
-        references: ResolvedReferences,
-        writer: Writer
-    ) {
-        val generateCode = generateCode(module, references)
+    private fun addInitFiles(base: Path, pythonPackage: Path) {
+        var currentPackage = pythonPackage
+        while (base != currentPackage) {
+            currentPackage.resolve("__init__.py").toFile().createNewFile()
+            currentPackage = pythonPackage.parent
+        }
+    }
+
+    private fun compileModule(module: Module, writer: Writer) {
+        val generateCode = generateCode(module.node, module.references)
         val stdlib = """\
             int_to_string = str
         """.trimMargin()
-        val contents = stdlib + "\n" + serialise(generateCode) + "\nmain()\n"
+        val contents = stdlib + "\n" + serialise(generateCode) + "\n"
         writer.write(contents)
     }
 }
