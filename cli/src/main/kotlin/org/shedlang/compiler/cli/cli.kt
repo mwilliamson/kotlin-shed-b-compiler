73a8e5fe49ced3dec31a09345bcc37e136bd5030
Michael Williamson <mike@zwobble.org>
2017-07-30 23:14:10 +0100

    Add basic compiler

--- cli/src/main/kotlin/org/shedlang/compiler/cli/cli.kt
+++ cli/src/main/kotlin/org/shedlang/compiler/cli/cli.kt
@@ -2,6 +2,29 @@
 
 package org.shedlang.compiler.cli
 
+import org.apache.commons.cli.CommandLine
+import org.apache.commons.cli.DefaultParser
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+import org.shedlang.compiler.backends.python.compile
+import org.shedlang.compiler.read
+import java.nio.file.Paths
+
 fun main(rawArguments: Array<String>) {
-    println("Hello, world")
+    val arguments = parseArguments(rawArguments)
+    for (arg in arguments.args) {
+        val result = read(
+            base = Paths.get("."),
+            path = Paths.get(arg)
+        )
+        compile(result, target = Paths.get(arguments.getOptionValue("o")))
+    }
+}
+
+private fun parseArguments(rawArguments: Array<String>): CommandLine {
+    val options = Options()
+
+    options.addOption(Option.builder("o").hasArg().required().build())
+
+    return DefaultParser().parse(options, rawArguments)
 }
