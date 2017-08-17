8575d35d93810c825f2dc66e2c5482ec237c900d
Michael Williamson <mike@zwobble.org>
2017-08-17 18:17:03 +0100

    Extract call parsing tests

--- frontend/src/test/kotlin/org/shedlang/compiler/tests/parser/ParseBinaryExpressionTests.kt
+++ frontend/src/test/kotlin/org/shedlang/compiler/tests/parser/ParseBinaryExpressionTests.kt
@@ -1,15 +1,9 @@
 package org.shedlang.compiler.tests.parser
 
 import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
-import com.natpryce.hamkrest.has
-import com.natpryce.hamkrest.throws
 import org.junit.jupiter.api.Test
 import org.shedlang.compiler.ast.Operator
-import org.shedlang.compiler.parser.ParseError
-import org.shedlang.compiler.parser.UnexpectedTokenException
 import org.shedlang.compiler.parser.parseExpression
-import org.shedlang.compiler.tests.isSequence
 
 class ParseBinaryExpressionTests {
     @Test
@@ -54,88 +48,6 @@ class ParseBinaryExpressionTests {
             isVariableReference("x"),
             isVariableReference("y")
         ))
-    }
-
-    @Test
-    fun canParseFunctionCallWithNoArguments() {
-        val source = "x()"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            isVariableReference("x"),
-            isSequence()
-        ))
-    }
-
-    @Test
-    fun canParseFunctionCallWithOneArgument() {
-        val source = "x(y)"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            isVariableReference("x"),
-            isSequence(isVariableReference("y"))
-        ))
-    }
-
-    @Test
-    fun canParseFunctionCallWithManyArguments() {
-        val source = "x(y, z)"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            isVariableReference("x"),
-            isSequence(isVariableReference("y"), isVariableReference("z"))
-        ))
-    }
-
-    @Test
-    fun canParseFunctionCallWithNamedArgument() {
-        val source = "x(y=z)"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            receiver = isVariableReference("x"),
-            positionalArguments = isSequence(),
-            namedArguments = isSequence(
-                isCallNamedArgument(
-                    name = equalTo("y"),
-                    expression = isVariableReference("z")
-                )
-            )
-        ))
-    }
-
-    @Test
-    fun positionalArgumentCannotAppearAfterNamedArgument() {
-        val source = "f(x=y, z)"
-        assertThat(
-            { parseString(::parseExpression, source) },
-            throws(has(ParseError::message, equalTo("Positional argument cannot appear after named argument")))
-        )
-    }
-
-    @Test
-    fun canParseFunctionCallWithExplicitTypeArgument() {
-        val source = "f[T]()"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            typeArguments = isSequence(isTypeReference("T"))
-        ))
-    }
-
-    @Test
-    fun canParseFunctionCallWithExplicitTypeArguments() {
-        val source = "f[T, U]()"
-        val node = parseString(::parseExpression, source)
-        assertThat(node, isCall(
-            typeArguments = isSequence(isTypeReference("T"), isTypeReference("U"))
-        ))
-    }
-
-    @Test
-    fun explicitTypeArgumentsMustHaveAtLeastOneArgument() {
-        val source = "f[]()"
-        assertThat(
-            { parseString(::parseExpression, source) },
-            throws<UnexpectedTokenException>()
-        )
     }
 
     @Test
---
+++ frontend/src/test/kotlin/org/shedlang/compiler/tests/parser/ParseCallTests.kt
@@ -0,0 +1,95 @@
+package org.shedlang.compiler.tests.parser
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.has
+import com.natpryce.hamkrest.throws
+import org.junit.jupiter.api.Test
+import org.shedlang.compiler.parser.ParseError
+import org.shedlang.compiler.parser.UnexpectedTokenException
+import org.shedlang.compiler.parser.parseExpression
+import org.shedlang.compiler.tests.isSequence
+
+class ParseCallTests {
+    @Test
+    fun canParseFunctionCallWithNoArguments() {
+        val source = "x()"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            isVariableReference("x"),
+            isSequence()
+        ))
+    }
+
+    @Test
+    fun canParseFunctionCallWithOneArgument() {
+        val source = "x(y)"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            isVariableReference("x"),
+            isSequence(isVariableReference("y"))
+        ))
+    }
+
+    @Test
+    fun canParseFunctionCallWithManyArguments() {
+        val source = "x(y, z)"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            isVariableReference("x"),
+            isSequence(isVariableReference("y"), isVariableReference("z"))
+        ))
+    }
+
+    @Test
+    fun canParseFunctionCallWithNamedArgument() {
+        val source = "x(y=z)"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            receiver = isVariableReference("x"),
+            positionalArguments = isSequence(),
+            namedArguments = isSequence(
+                isCallNamedArgument(
+                    name = equalTo("y"),
+                    expression = isVariableReference("z")
+                )
+            )
+        ))
+    }
+
+    @Test
+    fun positionalArgumentCannotAppearAfterNamedArgument() {
+        val source = "f(x=y, z)"
+        assertThat(
+            { parseString(::parseExpression, source) },
+            throws(has(ParseError::message, equalTo("Positional argument cannot appear after named argument")))
+        )
+    }
+
+    @Test
+    fun canParseFunctionCallWithExplicitTypeArgument() {
+        val source = "f[T]()"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            typeArguments = isSequence(isTypeReference("T"))
+        ))
+    }
+
+    @Test
+    fun canParseFunctionCallWithExplicitTypeArguments() {
+        val source = "f[T, U]()"
+        val node = parseString(::parseExpression, source)
+        assertThat(node, isCall(
+            typeArguments = isSequence(isTypeReference("T"), isTypeReference("U"))
+        ))
+    }
+
+    @Test
+    fun explicitTypeArgumentsMustHaveAtLeastOneArgument() {
+        val source = "f[]()"
+        assertThat(
+            { parseString(::parseExpression, source) },
+            throws<UnexpectedTokenException>()
+        )
+    }
+}
