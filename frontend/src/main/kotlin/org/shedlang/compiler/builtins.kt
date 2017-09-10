1d6d40e908e4470a824078ab1df337ed45d8a9f6
Michael Williamson <mike@zwobble.org>
2017-09-10 10:45:27 +0100

    Specify map and forEach types using shed syntax

--- frontend/src/main/kotlin/org/shedlang/compiler/builtins.kt
+++ frontend/src/main/kotlin/org/shedlang/compiler/builtins.kt
@@ -1,6 +1,10 @@
 package org.shedlang.compiler
 
 import org.shedlang.compiler.ast.freshNodeId
+import org.shedlang.compiler.parser.parse
+import org.shedlang.compiler.typechecker.evalType
+import org.shedlang.compiler.typechecker.newTypeContext
+import org.shedlang.compiler.typechecker.resolve
 import org.shedlang.compiler.types.*
 
 internal data class Builtin(
@@ -10,47 +14,39 @@ internal data class Builtin(
     val nodeId = freshNodeId()
 }
 
-private val mapFromType = TypeParameter("T", variance = Variance.INVARIANT)
-private val mapToType = TypeParameter("R", variance = Variance.INVARIANT)
-private val mapEffectParameter = EffectParameter("E")
-private val mapType = functionType(
-    staticParameters = listOf(mapFromType, mapToType, mapEffectParameter),
-    positionalArguments = listOf(
-        functionType(
-            positionalArguments = listOf(mapFromType),
-            effect = mapEffectParameter,
-            returns = mapToType
-        ),
-        applyType(ListType, listOf(mapFromType))
-    ),
-    effect = mapEffectParameter,
-    returns = applyType(ListType, listOf(mapToType))
-)
-
-private val forEachTypeParameter = TypeParameter("T", variance = Variance.INVARIANT)
-private val forEachEffectParameter = EffectParameter("E")
-private val forEachType = functionType(
-    staticParameters = listOf(forEachTypeParameter, forEachEffectParameter),
-    positionalArguments = listOf(
-        functionType(
-            positionalArguments = listOf(forEachTypeParameter),
-            effect = forEachEffectParameter,
-            returns = UnitType
-        ),
-        applyType(ListType, listOf(forEachTypeParameter))
-    ),
-    effect = forEachEffectParameter,
-    returns = UnitType
-)
-
-internal val builtins = listOf(
+private val coreBuiltins = listOf(
     Builtin("Any", MetaType(AnyType)),
     Builtin("Unit", MetaType(UnitType)),
     Builtin("Int", MetaType(IntType)),
     Builtin("String", MetaType(StringType)),
     Builtin("Bool", MetaType(BoolType)),
-    Builtin("List", MetaType(ListType)),
+    Builtin("List", MetaType(ListType))
+)
 
+fun parseType(string: String): Type {
+    val node = parse(
+        filename = "<string>",
+        input = string,
+        rule = { tokens -> org.shedlang.compiler.parser.parseType(tokens) }
+    )
+    val resolvedReferences = resolve(
+        node,
+        coreBuiltins.associate({ builtin -> builtin.name to builtin.nodeId})
+    )
+
+    val typeContext = newTypeContext(
+        nodeTypes = coreBuiltins.associate({ builtin -> builtin.nodeId to builtin.type}),
+        resolvedReferences = resolvedReferences,
+        getModule = { importPath -> throw UnsupportedOperationException() }
+    )
+
+    return evalType(node, typeContext)
+}
+
+private val mapType = parseType("[T, R, !E]((T) !E -> T, List[T]) !E -> List[R]")
+private val forEachType = parseType("[T, !E]((T) !E -> Unit, List[T]) !E -> Unit")
+
+internal val builtins = coreBuiltins + listOf(
     Builtin("!io", EffectType(IoEffect)),
 
     Builtin("print", FunctionType(
