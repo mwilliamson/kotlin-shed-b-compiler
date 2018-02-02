b77127ca3b1213cf79601839de8de1b5891e081d
Michael Williamson <mike@zwobble.org>
2018-02-02 20:53:27 +0000

    Print nicer errors from CLI

--- cli/src/main/kotlin/org/shedlang/compiler/cli/cli.kt
+++ cli/src/main/kotlin/org/shedlang/compiler/cli/cli.kt
@@ -2,8 +2,12 @@ package org.shedlang.compiler.cli
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
+import org.shedlang.compiler.backends.Backend
 import org.shedlang.compiler.readPackage
+import org.shedlang.compiler.typechecker.TypeCheckError
+import java.nio.file.Path
 import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 object ShedCli {
     @JvmStatic
@@ -14,14 +18,18 @@ object ShedCli {
         System.exit(exitCode)
     }
 
-    fun run(rawArguments: Array<String>): Int {
+    private fun run(rawArguments: Array<String>): Int {
         val arguments = Arguments(ArgParser(rawArguments))
         val mainName = arguments.mainModule.split(".")
-        val result = readPackage(Paths.get(arguments.source), mainName)
 
         val tempDir = createTempDir()
         try {
-            arguments.backend.compile(result, target = tempDir.toPath())
+            compile(
+                base = Paths.get(arguments.source),
+                mainName = arguments.mainModule,
+                backend = arguments.backend,
+                target = tempDir.toPath()
+            )
             return arguments.backend.run(tempDir.toPath(), mainName)
         } finally {
             tempDir.deleteRecursively()
@@ -49,9 +57,12 @@ object ShedcCli {
 
     private fun run(rawArguments: Array<String>) {
         val arguments = Arguments(ArgParser(rawArguments))
-        val mainName = arguments.mainModule.split(".")
-        val result = readPackage(Paths.get(arguments.source), mainName)
-        arguments.backend.compile(result, target = Paths.get(arguments.outputPath))
+        compile(
+            base = Paths.get(arguments.source),
+            mainName = arguments.mainModule,
+            backend = arguments.backend,
+            target = Paths.get(arguments.outputPath)
+        )
     }
 
     private class Arguments(parser: ArgParser) {
@@ -63,5 +74,16 @@ object ShedcCli {
         init {
             parser.force()
         }
+    }
+}
+
+private fun compile(base: Path, mainName: String, backend: Backend, target: Path) {
+    try {
+        val result = readPackage(base, mainName.split("."))
+        backend.compile(result, target = target)
+    } catch (error: TypeCheckError) {
+        System.err.println("Error: " + error.message)
+        System.err.println(error.source.describe())
+        exitProcess(2)
     }
 }
