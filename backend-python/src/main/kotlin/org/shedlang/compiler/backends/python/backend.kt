bcc6e591e3832a2775078889582fecc01e3001af
Michael Williamson <mike@zwobble.org>
2018-02-16 22:34:25 +0000

    Fix indentation

--- backend-python/src/main/kotlin/org/shedlang/compiler/backends/python/backend.kt
+++ backend-python/src/main/kotlin/org/shedlang/compiler/backends/python/backend.kt
@@ -95,35 +95,35 @@ private class PythonModule(val name: List<String>, val source: String)
 private val builtinModule = PythonModule(
     name = listOf("shed", "builtins"),
     source = """
-         from __future__ import print_function
+        from __future__ import print_function
 
-         from functools import partial
+        from functools import partial
 
-         int_to_string = str
+        int_to_string = str
 
-         _list = list
-         def list(*args):
-             return _list(args)
+        _list = list
+        def list(*args):
+            return _list(args)
 
-         _print = print
-         def print(value):
-             _print(value, end="")
+        _print = print
+        def print(value):
+            _print(value, end="")
 
-         def for_each(func, elements):
-             for element in elements:
-                 func(element)
+        def for_each(func, elements):
+            for element in elements:
+                func(element)
 
-         def map(func, elements):
-             result = []
-             for element in elements:
-                 result.append(func(element))
-             return result
+        def map(func, elements):
+            result = []
+            for element in elements:
+                result.append(func(element))
+            return result
 
-         def reduce(func, initial, elements):
-             result = initial
-             for element in elements:
-                 result = func(result, element)
-             return result
+        def reduce(func, initial, elements):
+            result = initial
+            for element in elements:
+                result = func(result, element)
+            return result
 
     """.trimIndent()
 )
