7b2abb60752cfeebd268127b07500722e8b7df71
Michael Williamson <mike@zwobble.org>
2018-07-28 17:49:12 +0100

    Add stubbed native modules

--- interpreter/src/main/kotlin/org/shedlang/compiler/interpreter/NativeModules.kt
+++ interpreter/src/main/kotlin/org/shedlang/compiler/interpreter/NativeModules.kt
@@ -2,4 +2,23 @@ package org.shedlang.compiler.interpreter
 
 import org.shedlang.compiler.ast.Identifier
 
-internal val nativeModules: Map<List<Identifier>, ModuleExpression> = mapOf()
+private val listsModule = ModuleExpression(
+    fieldExpressions = listOf(),
+    fieldValues = listOf()
+)
+
+private val stringsModule = ModuleExpression(
+    fieldExpressions = listOf(),
+    fieldValues = listOf()
+)
+
+private val typesModule = ModuleExpression(
+    fieldExpressions = listOf(),
+    fieldValues = listOf()
+)
+
+internal val nativeModules: Map<List<Identifier>, ModuleExpression> = mapOf(
+    listOf(Identifier("stdlib"), Identifier("platform"), Identifier("Lists")) to listsModule,
+    listOf(Identifier("stdlib"), Identifier("platform"), Identifier("Strings")) to stringsModule,
+    listOf(Identifier("stdlib"), Identifier("platform"), Identifier("Types")) to typesModule
+)
