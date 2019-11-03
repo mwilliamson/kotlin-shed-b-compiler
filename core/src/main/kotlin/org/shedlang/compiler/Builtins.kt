37d84b82a9e89de18eafcd53b863f361b392d5b8
Michael Williamson <mike@zwobble.org>
2019-11-03 22:36:20 +0000

    Define builtins in single module

---
+++ core/src/main/kotlin/org/shedlang/compiler/Builtins.kt
@@ -0,0 +1,32 @@
+package org.shedlang.compiler
+
+import org.shedlang.compiler.ast.*
+import org.shedlang.compiler.types.*
+
+fun isCast(call: CallBaseNode, references: ResolvedReferences): Boolean {
+    val receiver = call.receiver
+    return receiver is ReferenceNode && references[receiver] == Builtins.castBuiltin
+}
+
+object Builtins {
+    val castBuiltin = builtinVariable("cast", CastType)
+    val moduleName = builtinVariable("moduleName", StringType)
+}
+
+val builtins = listOf(
+    builtinType("Any", AnyType),
+    builtinType("Nothing", NothingType),
+    builtinType("Unit", UnitType),
+    builtinType("Int", IntType),
+    builtinType("String", StringType),
+    builtinType("CodePoint", CodePointType),
+    builtinType("Bool", BoolType),
+    builtinType("Symbol", AnySymbolType),
+    builtinType("ShapeField", ShapeFieldTypeFunction),
+
+    builtinEffect("Io", IoEffect),
+    builtinEffect("Pure", EmptyEffect),
+
+    Builtins.castBuiltin,
+    Builtins.moduleName
+)
