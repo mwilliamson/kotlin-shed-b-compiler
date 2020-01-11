6b154d407c601d5c8284f042099a7ac19a0f4b26
Michael Williamson <mike@zwobble.org>
2020-01-11 00:11:36 +0000

    Hack together basic LLVM compiler

--- backend-llvm/src/main/kotlin/org/shedlang/compiler/backends/llvm/LlvmIr.kt
+++ backend-llvm/src/main/kotlin/org/shedlang/compiler/backends/llvm/LlvmIr.kt
@@ -1,17 +1,56 @@
 package org.shedlang.compiler.backends.llvm
 
-internal data class LlvmType(val name: String) {
-    fun serialise(): String {
+internal interface LlvmType {
+    fun serialise(): String
+}
+
+internal data class LlvmTypeScalar(val name: String): LlvmType {
+    override fun serialise(): String {
         return name
     }
 }
 
+internal data class LlvmTypePointer(val type: LlvmType): LlvmType {
+    override fun serialise(): String {
+        return "${type.serialise()} *"
+    }
+}
+
+internal data class LlvmTypeArray(val size: Int, val elementType: LlvmType): LlvmType {
+    override fun serialise(): String {
+        return "[$size x ${elementType.serialise()}]"
+    }
+}
+
+internal data class LlvmTypeFunction(val returnType: LlvmType): LlvmType {
+    override fun serialise(): String {
+        return "${returnType.serialise()} ()"
+    }
+}
+
 internal object LlvmTypes {
-    val i32 = LlvmType("i32")
+    val i8 = LlvmTypeScalar("i8")
+    val i32 = LlvmTypeScalar("i32")
+    val i64 = LlvmTypeScalar("i64")
+    val void = LlvmTypeScalar("void")
+
+    fun pointer(type: LlvmType) = LlvmTypePointer(type = type)
+
+    fun arrayType(size: Int, elementType: LlvmType) = LlvmTypeArray(size = size, elementType = elementType)
+
+    fun function(returnType: LlvmType): LlvmType = LlvmTypeFunction(returnType = returnType)
 }
 
 internal interface LlvmOperand {
     fun serialise(): String
+}
+
+internal interface LlvmVariable: LlvmOperand
+
+internal object LlvmNullPointer: LlvmOperand {
+    override fun serialise(): String {
+        return "null"
+    }
 }
 
 internal data class LlvmOperandInt(val value: Int): LlvmOperand {
@@ -20,8 +59,118 @@ internal data class LlvmOperandInt(val value: Int): LlvmOperand {
     }
 }
 
+internal data class LlvmOperandGlobal(val name: String): LlvmVariable {
+    override fun serialise(): String {
+        return "@$name"
+    }
+}
+
+internal data class LlvmOperandLocal(val name: String): LlvmVariable {
+    override fun serialise(): String {
+        return "%$name"
+    }
+}
+
+internal data class LlvmTypedOperand(val type: LlvmType, val operand: LlvmOperand) {
+    fun serialise(): String {
+        return "${type.serialise()} ${operand.serialise()}"
+    }
+}
+
 internal interface LlvmBasicBlock {
     fun serialise(): String
+}
+
+internal data class LlvmBitCast(
+    val target: LlvmVariable,
+    val sourceType: LlvmType,
+    val value: LlvmOperand,
+    val targetType: LlvmType
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = bitcast ${sourceType.serialise()} ${value.serialise()} to ${targetType.serialise()}"
+    }
+}
+
+internal data class LlvmIntToPtr(
+    val target: LlvmVariable,
+    val sourceType: LlvmType,
+    val value: LlvmOperand,
+    val targetType: LlvmType
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = inttoptr ${sourceType.serialise()} ${value.serialise()} to ${targetType.serialise()}"
+    }
+}
+
+internal data class LlvmPtrToInt(
+    val target: LlvmVariable,
+    val sourceType: LlvmType,
+    val value: LlvmOperand,
+    val targetType: LlvmType
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = ptrtoint ${sourceType.serialise()} ${value.serialise()} to ${targetType.serialise()}"
+    }
+}
+
+internal data class LlvmCall(
+    val target: LlvmVariable?,
+    val returnType: LlvmType,
+    val functionPointer: LlvmOperand,
+    val arguments: List<LlvmTypedOperand>
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        val prefix = if (target == null) { "" } else { "${target.serialise()} = " }
+        val argumentsString = arguments.joinToString(", ") { argument -> argument.serialise() }
+        return "${prefix}call ${returnType.serialise()} ${functionPointer.serialise()}($argumentsString)"
+    }
+}
+
+internal data class LlvmAssign(
+    val target: LlvmVariable,
+    val value: LlvmOperand
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = ${value.serialise()}"
+    }
+
+}
+
+internal data class LlvmExtractValue(
+    val target: LlvmVariable,
+    val valueType: LlvmType,
+    val value: LlvmOperand,
+    val index: LlvmOperand
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = extractvalue ${valueType.serialise()} ${value.serialise()}, ${index.serialise()}"
+    }
+}
+
+internal data class LlvmGetElementPtr(
+    val target: LlvmVariable,
+    val type: LlvmType,
+    val pointer: LlvmOperand,
+    val indices: List<LlvmIndex>
+): LlvmBasicBlock {
+    override fun serialise(): String {
+        val indicesString = indices.joinToString("") {index -> ", ${index.serialise()}" }
+
+        return "${target.serialise()} = getelementptr ${type.serialise()}, ${LlvmTypes.pointer(type).serialise()} ${pointer.serialise()}$indicesString"
+    }
+}
+
+internal data class LlvmIndex(val type: LlvmType, val value: LlvmOperand) {
+    fun serialise(): String {
+        return "${type.serialise()} ${value.serialise()}"
+    }
+}
+
+internal data class LlvmLoad(val target: LlvmVariable, val type: LlvmType, val pointerType: LlvmType, val pointer: LlvmOperand): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "${target.serialise()} = load ${type.serialise()}, ${pointerType.serialise()} ${pointer.serialise()}"
+    }
 }
 
 internal data class LlvmReturn(val type: LlvmType, val value: LlvmOperand): LlvmBasicBlock {
@@ -30,13 +179,45 @@ internal data class LlvmReturn(val type: LlvmType, val value: LlvmOperand): Llvm
     }
 }
 
+internal object LlvmReturnVoid: LlvmBasicBlock {
+    override fun serialise(): String {
+        return "ret void"
+    }
+}
+
+internal data class LlvmStore(val type: LlvmType, val value: LlvmOperand, val pointer: LlvmOperand): LlvmBasicBlock {
+    override fun serialise(): String {
+        return "store ${type.serialise()} ${value.serialise()}, ${LlvmTypes.pointer(type).serialise()} ${pointer.serialise()}"
+    }
+}
+
 internal data class LlvmFunctionDefinition(
     val name: String,
     val returnType: LlvmType,
     val body: List<LlvmBasicBlock>
-) {
-    fun serialise(): String {
+): LlvmModuleStatement {
+    override fun serialise(): String {
         val bodyString = body.joinToString("") { block -> "    " + block.serialise()+ "\n" }
         return "define ${returnType.serialise()} @$name() {\n$bodyString}\n"
     }
+}
+
+internal data class LlvmGlobalDefinition(
+    val name: String,
+    val type: LlvmType,
+    val value: LlvmOperand
+): LlvmModuleStatement {
+    override fun serialise(): String {
+        return "@$name = global ${type.serialise()} ${value.serialise()}\n"
+    }
+}
+
+internal data class LlvmModule(val body: List<LlvmModuleStatement>) {
+    fun serialise(): String {
+        return body.joinToString("") { statement -> statement.serialise() }
+    }
+}
+
+interface LlvmModuleStatement {
+    fun serialise(): String
 }
--- backend-llvm/src/test/kotlin/org/shedlang/compiler/backends/llvm/tests/ExecutionTests.kt
+++ backend-llvm/src/test/kotlin/org/shedlang/compiler/backends/llvm/tests/ExecutionTests.kt
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 import org.shedlang.compiler.ModuleSet
 import org.shedlang.compiler.ast.Identifier
-import org.shedlang.compiler.backends.llvm.LlvmFunctionDefinition
-import org.shedlang.compiler.backends.llvm.LlvmOperandInt
-import org.shedlang.compiler.backends.llvm.LlvmReturn
-import org.shedlang.compiler.backends.llvm.LlvmTypes
+import org.shedlang.compiler.backends.llvm.*
 import org.shedlang.compiler.backends.tests.temporaryDirectory
 import org.shedlang.compiler.backends.tests.testPrograms
+import org.shedlang.compiler.stackir.*
 import org.shedlang.compiler.typechecker.CompilerError
 import org.shedlang.compiler.typechecker.SourceError
+import org.shedlang.compiler.types.Type
 import java.nio.file.Path
 
 class ExecutionTests {
@@ -75,15 +74,320 @@ class ExecutionTests {
 }
 
 private class Compiler(private val moduleSet: ModuleSet) {
+    private val image = loadModuleSet(moduleSet)
+    
     fun compile(target: Path, mainModule: List<Identifier>) {
+        val defineMainModule = moduleDefine(mainModule)
+
+        val mainModuleVariable = LlvmOperandLocal("mainModule")
+        val mainFunctionUntypedVariable = LlvmOperandLocal("mainFunctionUntyped")
+        val mainFunctionVariable = LlvmOperandLocal("mainFunction")
+        val exitCodeVariable = LlvmOperandLocal("exitCode")
         val main = LlvmFunctionDefinition(
-            returnType = LlvmTypes.i32,
+            returnType = LlvmTypes.i64,
             name = "main",
             body = listOf(
-                LlvmReturn(type = LlvmTypes.i32, value = LlvmOperandInt(42))
-            )
+                importModule(mainModule, target = mainModuleVariable),
+                fieldAccess(
+                    mainModuleVariable,
+                    Identifier("main"),
+                    receiverType = moduleSet.module(mainModule)!!.type,
+                    target = mainFunctionUntypedVariable
+                ),
+                listOf(
+                    LlvmIntToPtr(
+                        target = mainFunctionVariable,
+                        sourceType = LlvmTypes.i64,
+                        value = mainFunctionUntypedVariable,
+                        targetType = LlvmTypes.pointer(LlvmTypes.function(
+                            returnType = LlvmTypes.i64
+                        ))
+                    ),
+                    LlvmCall(
+                        target = exitCodeVariable,
+                        returnType = LlvmTypes.i64,
+                        functionPointer = mainFunctionVariable,
+                        arguments = listOf()
+                    ),
+                    LlvmReturn(type = LlvmTypes.i64, value = exitCodeVariable)
+                )
+            ).flatten()
         )
 
-        target.toFile().writeText(main.serialise())
+        val module = LlvmModule(
+            listOf(
+                defineMainModule,
+                listOf(main)
+            ).flatten()
+        )
+
+        // TODO: handle malloc declaration properly
+        val source = "declare i8* @malloc(i64)\n" + module.serialise()
+
+        println(source.lines().mapIndexed { index, line ->
+            (index + 1).toString().padStart(3) + " " + line
+        }.joinToString("\n"))
+
+        target.toFile().writeText(source)
+    }
+
+    private fun moduleDefine(moduleName: List<Identifier>): List<LlvmModuleStatement> {
+        return listOf(
+            LlvmGlobalDefinition(
+                name = nameForModuleValue(moduleName),
+                type = objectType,
+                value = LlvmNullPointer
+            )
+        ) + moduleInit(moduleName)
+    }
+
+    private fun moduleInit(moduleName: List<Identifier>): List<LlvmModuleStatement> {
+        return compileInstructions(image.moduleInitialisation(moduleName), stack = mutableListOf(), locals = mutableMapOf()).mapValue<LlvmModuleStatement> { instructions ->
+            LlvmFunctionDefinition(
+                name = nameForModuleInit(moduleName),
+                returnType = LlvmTypes.void,
+                body = instructions + listOf(LlvmReturnVoid)
+            )
+        }.toModuleStatements()
+    }
+
+    private fun compileInstructions(instructions: List<Instruction>, stack: MutableList<LlvmOperand>, locals: MutableMap<Int, LlvmOperand>): CompilationResult<List<LlvmBasicBlock>> {
+        return CompilationResult.flatten(instructions.map { instruction -> compileInstruction(instruction, stack = stack, locals = locals) })
+    }
+
+    private fun compileInstruction(instruction: Instruction, stack: MutableList<LlvmOperand>, locals: MutableMap<Int, LlvmOperand>): CompilationResult<List<LlvmBasicBlock>> {
+        when (instruction) {
+            is DeclareFunction -> {
+                val functionName = generateName("function")
+                val functionPointerVariable = LlvmOperandLocal(generateName("functionPointer"))
+                return compileInstructions(instruction.bodyInstructions, stack = mutableListOf(), locals = mutableMapOf())
+                    .flatMapValue { instructions ->
+                        val functionDefinition = LlvmFunctionDefinition(
+                            name = functionName,
+                            returnType = LlvmTypes.i64,
+                            body = instructions
+                        )
+
+                        stack.add(functionPointerVariable)
+
+                        val getVariableAddress = LlvmPtrToInt(
+                            target = functionPointerVariable,
+                            targetType = LlvmTypes.i64,
+                            value = LlvmOperandGlobal(functionName),
+                            sourceType = LlvmTypes.pointer(LlvmTypes.function(returnType = LlvmTypes.i64))
+                        )
+                        CompilationResult.of(listOf<LlvmBasicBlock>(getVariableAddress))
+                            .addModuleStatements(listOf(functionDefinition))
+                    }
+            }
+
+            is Exit -> {
+                return CompilationResult.of(listOf())
+            }
+
+            is LocalStore -> {
+                val operand = stack.pop()
+                locals.put(instruction.variableId, operand)
+                return CompilationResult.of(listOf())
+            }
+
+            is ModuleStore -> {
+                val moduleVariableUntyped = LlvmOperandLocal(generateName("moduleUntyped"))
+                val moduleVariable = LlvmOperandLocal(generateName("module"))
+                val fieldVariable = LlvmOperandLocal(generateName("field"))
+                val (exportName, exportVariableId) = instruction.exports.single()
+                return CompilationResult.of(listOf(
+                    LlvmCall(
+                        target = moduleVariableUntyped,
+                        returnType = LlvmTypes.pointer(LlvmTypes.i8),
+                        functionPointer = LlvmOperandGlobal("malloc"),
+                        arguments = listOf(
+                            LlvmTypedOperand(LlvmTypes.i64, LlvmOperandInt(8 * instruction.exports.size))
+                        )
+                    ),
+                    LlvmBitCast(
+                        target = moduleVariable,
+                        sourceType = LlvmTypes.pointer(LlvmTypes.i8),
+                        value = moduleVariableUntyped,
+                        targetType = objectType
+                    ),
+                    // TODO: don't assume exactly one export
+                    LlvmGetElementPtr(
+                        target = fieldVariable,
+                        type = objectType.type,
+                        pointer = moduleVariable,
+                        indices = listOf(
+                            LlvmIndex(LlvmTypes.i64, LlvmOperandInt(0)),
+                            LlvmIndex(LlvmTypes.i64, LlvmOperandInt(0))
+                        )
+                    ),
+                    LlvmStore(
+                        type = LlvmTypes.i64,
+                        value = locals.getValue(exportVariableId),
+                        pointer = fieldVariable
+                    ),
+                    LlvmStore(
+                        type = objectType,
+                        value = moduleVariable,
+                        pointer = operandForModuleValue(instruction.moduleName)
+                    )
+                ))
+            }
+
+            is PushValue -> {
+                stack.add(generateOperand(instruction.value))
+
+                return CompilationResult.of(listOf())
+            }
+
+            is Return -> {
+                val returnVariable = stack.pop()
+
+                return CompilationResult.of(listOf(
+                    LlvmReturn(type = LlvmTypes.i64, value = returnVariable)
+                ))
+            }
+
+            else -> {
+                throw UnsupportedOperationException(instruction.toString())
+            }
+        }
+    }
+
+    private fun variableForLocal(variableId: Int): LlvmVariable {
+        return LlvmOperandLocal("local_$variableId")
+    }
+
+    private fun generateOperand(value: IrValue): LlvmOperand {
+        if (value is IrInt) {
+            return LlvmOperandInt(value.value.intValueExact())
+        } else {
+            throw UnsupportedOperationException(value.toString())
+        }
+    }
+
+    private fun fieldAccess(receiver: LlvmOperand, fieldName: Identifier, receiverType: Type, target: LlvmVariable): List<LlvmBasicBlock> {
+        val fieldPointerVariable = LlvmOperandLocal("fieldPointer")
+
+        // TODO: calculate fieldIndex
+        val fieldIndex = 0
+
+        return listOf(
+            LlvmGetElementPtr(
+                target = fieldPointerVariable,
+                type = objectType.type,
+                pointer = receiver,
+                indices = listOf(
+                    LlvmIndex(LlvmTypes.i32, LlvmOperandInt(0)),
+                    LlvmIndex(LlvmTypes.i32, LlvmOperandInt(fieldIndex))
+                )
+            ),
+            LlvmLoad(
+                target = target,
+                type = LlvmTypes.i64,
+                pointerType = LlvmTypes.pointer(LlvmTypes.i64),
+                pointer = fieldPointerVariable
+            )
+        )
+    }
+
+    private fun importModule(moduleName: List<Identifier>, target: LlvmVariable): List<LlvmBasicBlock> {
+        return listOf(
+            LlvmCall(
+                target = null,
+                returnType = LlvmTypes.void,
+                functionPointer = operandForModuleInit(moduleName),
+                arguments = listOf()
+            ),
+            LlvmLoad(
+                target = target,
+                type = objectType,
+                pointerType = LlvmTypes.pointer(objectType),
+                pointer = operandForModuleValue(moduleName)
+            )
+        )
+    }
+
+    private fun operandForModuleInit(moduleName: List<Identifier>): LlvmOperand {
+        return LlvmOperandGlobal(nameForModuleInit(moduleName))
+    }
+
+    private fun operandForModuleValue(moduleName: List<Identifier>): LlvmVariable {
+        return LlvmOperandGlobal(nameForModuleValue(moduleName))
+    }
+
+    private fun nameForModuleInit(moduleName: List<Identifier>): String {
+        return "shed__module_init__${serialiseModuleName(moduleName)}"
+    }
+
+    private fun nameForModuleValue(moduleName: List<Identifier>): String {
+        return "shed__module_value__${serialiseModuleName(moduleName)}"
+    }
+
+    private fun serialiseModuleName(moduleName: List<Identifier>) =
+        moduleName.joinToString("_") { part -> part.value }
+
+    var nextNameIndex = 1
+
+    private fun generateName(prefix: String): String {
+        return prefix + "_" + nextNameIndex++
+    }
+
+    private val objectType = LlvmTypes.pointer(LlvmTypes.arrayType(size = 0, elementType = LlvmTypes.i64))
+
+    private class CompilationResult<T>(
+        val value: T,
+        val moduleStatements: List<LlvmModuleStatement>
+    ) {
+        fun <R> mapValue(func: (T) -> R): CompilationResult<R> {
+            return CompilationResult(
+                value = func(value),
+                moduleStatements = moduleStatements
+            )
+        }
+
+        fun <R> flatMapValue(func: (T) -> CompilationResult<R>): CompilationResult<R> {
+            val result = func(value)
+            return CompilationResult(
+                value = result.value,
+                moduleStatements = moduleStatements + result.moduleStatements
+            )
+        }
+
+        fun addModuleStatements(moduleStatements: List<LlvmModuleStatement>): CompilationResult<T> {
+            return CompilationResult(
+                value = value,
+                moduleStatements = this.moduleStatements + moduleStatements
+            )
+        }
+
+        companion object {
+            val EMPTY = CompilationResult<List<Nothing>>(
+                value = listOf(),
+                moduleStatements = listOf()
+            )
+
+            fun <T> of(value: T): CompilationResult<T> {
+                return CompilationResult(
+                    value = value,
+                    moduleStatements = listOf()
+                )
+            }
+
+            fun <T> flatten(results: List<CompilationResult<List<T>>>): CompilationResult<List<T>> {
+                val value = results.flatMap { result -> result.value }
+                val moduleStatements = results.flatMap { result -> result.moduleStatements }
+                return CompilationResult(
+                    value = value,
+                    moduleStatements = moduleStatements
+                )
+            }
+        }
+    }
+
+    private fun CompilationResult<LlvmModuleStatement>.toModuleStatements(): List<LlvmModuleStatement> {
+        return moduleStatements + listOf(value)
     }
 }
+
+private fun <T> MutableList<T>.pop() = removeAt(lastIndex)
