9702293e2b0f7a1c65eea52b7f175ba3bd27524a
Michael Williamson <mike@zwobble.org>
2020-02-09 13:19:25 +0000

    Remove unused code

--- stack-interpreter/src/test/kotlin/org/shedlang/compiler/stackinterpreter/tests/StackInterpreterBuilders.kt
+++ stack-interpreter/src/test/kotlin/org/shedlang/compiler/stackinterpreter/tests/StackInterpreterBuilders.kt
@@ -1,26 +1,10 @@
 package org.shedlang.compiler.stackinterpreter.tests
 
-import org.shedlang.compiler.EMPTY_TYPES
-import org.shedlang.compiler.ResolvedReferences
-import org.shedlang.compiler.Types
-import org.shedlang.compiler.ast.Block
-import org.shedlang.compiler.ast.ExpressionNode
-import org.shedlang.compiler.backends.tests.loader
 import org.shedlang.compiler.stackinterpreter.InterpreterValue
 import org.shedlang.compiler.stackinterpreter.NullWorld
 import org.shedlang.compiler.stackinterpreter.World
 import org.shedlang.compiler.stackir.Image
 import org.shedlang.compiler.stackir.Instruction
-
-internal fun evaluateBlock(block: Block, references: ResolvedReferences): InterpreterValue {
-    val instructions = loader(references = references).loadBlock(block)
-    return executeInstructions(instructions)
-}
-
-internal fun evaluateExpression(node: ExpressionNode, types: Types = EMPTY_TYPES): InterpreterValue {
-    val instructions = loader(types = types).loadExpression(node)
-    return executeInstructions(instructions)
-}
 
 internal fun executeInstructions(
     instructions: List<Instruction>,
