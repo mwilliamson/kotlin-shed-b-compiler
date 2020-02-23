68b280c14298cedcbf6cd390355488469ed03de4
Michael Williamson <mike@zwobble.org>
2020-02-23 11:59:06 +0000

    Set correct receiver type in callFunction()

--- backend-testing/src/main/kotlin/org/shedlang/compiler/backends/tests/StackModuleTesting.kt
+++ backend-testing/src/main/kotlin/org/shedlang/compiler/backends/tests/StackModuleTesting.kt
@@ -1,15 +1,11 @@
 package org.shedlang.compiler.backends.tests
 
 import kotlinx.collections.immutable.persistentListOf
-import org.shedlang.compiler.Module
-import org.shedlang.compiler.ModuleSet
 import org.shedlang.compiler.ast.Identifier
 import org.shedlang.compiler.ast.ModuleName
 import org.shedlang.compiler.findRoot
 import org.shedlang.compiler.readPackage
 import org.shedlang.compiler.stackir.*
-import org.shedlang.compiler.tests.moduleType
-import org.shedlang.compiler.types.AnyType
 import org.shedlang.compiler.types.Type
 
 internal fun callFunction(
@@ -19,22 +15,20 @@ internal fun callFunction(
     arguments: List<IrValue>,
     type: Type
 ): StackExecutionResult {
+    val moduleSet = readPackage(
+        base = findRoot().resolve("stdlib"),
+        name = moduleName
+    )
+
+    val moduleType = moduleSet.moduleType(moduleName)!!
+
     val instructions = persistentListOf(
         ModuleInit(moduleName),
         ModuleLoad(moduleName),
-        FieldAccess(Identifier(functionName), receiverType = AnyType)
+        FieldAccess(Identifier(functionName), receiverType = moduleType)
     )
         .addAll(arguments.map { argument -> PushValue(argument) })
         .add(Call(positionalArgumentCount = arguments.size, namedArgumentNames = listOf()))
-
-    val optionsModules = readPackage(
-        base = findRoot().resolve("stdlib"),
-        name = listOf(Identifier("Stdlib"), Identifier("Options"))
-    ).modules
-
-    val moduleSet = ModuleSet(optionsModules + listOf(
-        Module.Native(name = moduleName, type = moduleType())
-    ))
 
     return environment.executeInstructions(instructions, type = type, moduleSet = moduleSet)
 }
