4b4678b3dba08b5d0c0f6d1e655be7d32f033e3c
Michael Williamson <mike@zwobble.org>
2020-04-04 16:10:37 +0100

    Dynamically grow string builder

--- stdlib-llvm/src/Stdlib.Platform.StringBuilder.c
+++ stdlib-llvm/src/Stdlib.Platform.StringBuilder.c
@@ -5,20 +5,21 @@
 #include "./shed.h"
 
 struct StringBuilderStack {
-    ShedString string;
+    uint8_t* data;
+    StringLength length;
+    StringLength capacity;
     struct StringBuilderStack* next;
 };
 
 static struct StringBuilderStack* string_builder_string_stack = NULL;
 
 ShedString Shed_Stdlib_Platform_StringBuilder_build(ShedEnvironment env, struct ShedClosure* closure) {
-    // TODO: dynamically grow data
-
-    ShedString string_builder_string = alloc_string(1024 * 1024);
-    string_builder_string->length = 0;
+    StringLength initial_capacity = 16;
 
     struct StringBuilderStack* new_stack = GC_malloc(sizeof(struct StringBuilderStack));
-    new_stack->string = string_builder_string;
+    new_stack->data = GC_malloc(sizeof(uint8_t) * initial_capacity);
+    new_stack->length = 0;
+    new_stack->capacity = initial_capacity;
     new_stack->next = string_builder_string_stack;
     string_builder_string_stack = new_stack;
 
@@ -27,18 +28,43 @@ ShedString Shed_Stdlib_Platform_StringBuilder_build(ShedEnvironment env, struct 
 
     string_builder_string_stack = string_builder_string_stack->next;
 
-    return string_builder_string;
+    ShedString string = alloc_string(new_stack->length);
+    string->length = new_stack->length;
+    memcpy(
+        string->data,
+        new_stack->data,
+        new_stack->length
+    );
+    return string;
 }
 
 ShedValue Shed_Stdlib_Platform_StringBuilder_write(ShedEnvironment env, ShedString value) {
-    ShedString string = string_builder_string_stack->string;
+    StringLength new_length = string_builder_string_stack->length + value->length;
+
+    if (string_builder_string_stack->capacity < new_length) {
+        StringLength new_capacity = string_builder_string_stack->capacity;
+        while (new_capacity < new_length) {
+            new_capacity *= 2;
+        }
+
+        uint8_t* new_data = GC_malloc(sizeof(uint8_t) * new_capacity);
+        memcpy(
+            new_data,
+            string_builder_string_stack->data,
+            string_builder_string_stack->length
+        );
+
+        string_builder_string_stack->data = new_data;
+        string_builder_string_stack->capacity = new_capacity;
+    }
+
     memcpy(
-        &string->data[string->length],
+        &string_builder_string_stack->data[string_builder_string_stack->length],
         value->data,
         value->length
     );
 
-    string->length += value->length;
+    string_builder_string_stack->length = new_length;
 
     return shed_unit;
 }
