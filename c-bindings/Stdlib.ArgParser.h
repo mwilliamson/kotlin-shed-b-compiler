bfecc0d42ab3c99c656a2216160dbf9d39c0542e
Michael Williamson <mike@zwobble.org>
2020-08-25 18:52:42 +0100

    Add C bindings for Stdlib.ArgParser

---
+++ c-bindings/Stdlib.ArgParser.h
@@ -0,0 +1,9 @@
+#ifndef SHED__Stdlib_ArgParser_H
+#define SHED__Stdlib_ArgParser_H
+extern struct {
+    ShedValue parse;
+    ShedValue parser;
+    ShedValue positional;
+    ShedValue string;
+} shed__module_value__Stdlib_ArgParser;
+#endif
