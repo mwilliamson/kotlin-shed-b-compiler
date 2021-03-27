64a1ef466d9788d81c6845f40f266d76d68f3f73
Michael Williamson <mike@zwobble.org>
2021-03-27 15:26:06 +0000

    Add extra cases to intToString tests

--- backend-testing/src/main/kotlin/org/shedlang/compiler/backends/tests/StackIntToStringModuleTests.kt
+++ backend-testing/src/main/kotlin/org/shedlang/compiler/backends/tests/StackIntToStringModuleTests.kt
@@ -4,23 +4,43 @@ import com.natpryce.hamkrest.assertion.assertThat
 import org.junit.jupiter.api.Test
 import org.shedlang.compiler.ast.Identifier
 import org.shedlang.compiler.stackir.IrInt
+import org.shedlang.compiler.stackir.IrValue
 import org.shedlang.compiler.types.StringType
 
 abstract class StackIntToStringModuleTests(private val environment: StackIrExecutionEnvironment) {
     private val moduleName = listOf(Identifier("Core"), Identifier("IntToString"))
 
     @Test
+    fun zero() {
+        val result = intToString(0)
+
+        assertThat(result, isString("0"))
+    }
+
+    @Test
     fun smallPositiveInteger() {
+        val result = intToString(42)
+
+        assertThat(result, isString("42"))
+    }
+
+    @Test
+    fun smallNegativeInteger() {
+        val result = intToString(-42)
+
+        assertThat(result, isString("-42"))
+    }
+
+    private fun intToString(value: Int): IrValue {
         val result = callFunction(
             environment = environment,
             moduleName = moduleName,
             functionName = "intToString",
             arguments = listOf(
-                IrInt(42)
+                IrInt(value)
             ),
             type = StringType
         )
-
-        assertThat(result.value, isString("42"))
+        return result.value
     }
 }
