cb3ccd3108750f658448e217abcd6b5b06775415
Michael Williamson <mike@zwobble.org>
2021-04-19 20:18:14 +0100

    Handle incomplete writes

--- backend-llvm/runtime/src/Core.Io.c
+++ backend-llvm/runtime/src/Core.Io.c
@@ -1,8 +1,16 @@
+#include <stdbool.h>
 #include <unistd.h>
 
 #include "./shed.h"
 
 ShedAny shed_module_fun__Core__Io__print(ShedEnvironment env, ShedString value) {
-    write(STDOUT_FILENO, value->data, value->length);
+    size_t total_bytes_written = 0;
+    while (total_bytes_written < value->length) {
+        ssize_t bytes_written = write(STDOUT_FILENO, &value->data[total_bytes_written], value->length - total_bytes_written);
+        if (bytes_written == -1) {
+            break;
+        }
+        total_bytes_written += bytes_written;
+    }
     return shed_unit;
 }
